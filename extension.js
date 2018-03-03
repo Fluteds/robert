66571eca534a5042c49f2b6d5b2dd38ab3a23807
Alice <34608301+NotFluted@users.noreply.github.com>
2018-03-03 22:37:31 +0000

    Update extension.js
    Name Changes.

--- extension.js
+++ extension.js
@@ -1,8 +1,7 @@
 /**
 
  Copyright © 2017-2018 robert
- 
- Original Base of source code, basicBot. Developed by Benzi (https://github.com/benzi)
+
  Modifications (including forks) of the code to fit personal needs are allowed only for personal use and should refer back to the original source.
  This software is not for profit, any extension, or unauthorised person providing this software is not authorised to be in a position of any monetary gain from this use of this software. Any and all money gained under the use of the software (which includes donations) must be passed on to the original author.
 
@@ -262,7 +261,7 @@
         retrieveSettings: retrieveSettings,
         retrieveFromStorage: retrieveFromStorage,
         settings: {
-            botName: "robert",
+            botName: "Fluted Bot",
             language: "english",
             chatLink: "https://rawgit.com/NotFluted/robert/master/lang/en.json",
             scriptLink: "https://rawgit.com/NotFluted/robert/master/extension.js",
