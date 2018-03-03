7fd0ef0cd1a083d7b47e0ed7af8b7e58b72095ab
Alice <34608301+NotFluted@users.noreply.github.com>
2018-03-03 22:53:19 +0000

    Update extension.js
    Updated language .json files...again.

--- extension.js
+++ extension.js
@@ -112,7 +112,7 @@
 
     var loadChat = function(cb) {
         if (!cb) cb = function() {};
-        $.get("https://rawgit.com/NotFluted/robert/master/lang/langindex", function(json) {
+        $.get("https://rawgit.com/NotFluted/robert/master/lang/en.json", function(json) {
             var link = robert.chatLink;
             if (json !== null && typeof json !== 'undefined') {
                 langIndex = json;
@@ -255,7 +255,7 @@
         loggedInID: "33104203",
         scriptLink: "https://rawgit.com/NotFluted/robert/master/extension.js",
         cmdLink: "https://goo.gl/4BQP8Y",
-        chatLink: "https://rawgit.com/NotFluted/robert/master/lang/langindex",
+        chatLink: "https://rawgit.com/NotFluted/robert/master/lang/en.json",
         chat: null,
         loadChat: loadChat,
         retrieveSettings: retrieveSettings,
